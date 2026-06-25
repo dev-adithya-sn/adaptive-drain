5ac8876c9d65d420da838a2a89436bc1a2e9d5e0
S N Adithya Srivatsan <adithyasrivatsan.sn2025@vitstudent.ac.in>
2026-06-25 12:51:28 +0530

    Update server.py

--- server.py
+++ server.py
@@ -120,4 +120,4 @@ def get_stats():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050, debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5050)), debug=False)
